361ce46eb34fc0fcd0f9edb6f8715dfe2fbf68c7
epicstar <myramenisgone@gmail.com>
2014-07-30 08:30:03 -0400

    Testing to see if the serializable location actually works...

--- ClinicalTrials.gov/CT.gov/MeSHMapper.java
+++ ClinicalTrials.gov/CT.gov/MeSHMapper.java
@@ -1,4 +1,10 @@
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.HashMap;
+
 import mapping.RFFMapper;
 
 public class MeSHMapper {
@@ -11,6 +17,23 @@ public class MeSHMapper {
 		RFFMapper map = new RFFMapper();
 		map.makeMapping(inputdata, 1, 13, 14);
 		map.makeSerializable(serialLocation);
+		try {
+			ObjectInputStream mappu = new ObjectInputStream(new FileInputStream(serialLocation));
+			@SuppressWarnings("unchecked")
+			HashMap<String, String> hey = (HashMap<String, String>)mappu.readObject();
+			mappu.close();
+			
+			System.out.println(hey);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
 
