2ab199aa8d9c45b53cda18ca9b69ad20c20fa250
epicstar <myramenisgone@gmail.com>
2014-08-18 09:59:12 -0400

    Created a mostly functioning CUI adder using exact string mapping and Nobletools from Pitt's DBMI

--- ClinicalTrials.gov/CT.gov/map/addcuis/AddCuis.java
+++ ClinicalTrials.gov/CT.gov/map/addcuis/AddCuis.java
@@ -2,8 +2,8 @@ package map.addcuis;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.PrintWriter;
@@ -12,6 +12,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import edu.pitt.terminology.client.IndexFinderTerminology;
 import edu.pitt.terminology.lexicon.Concept;
@@ -34,12 +35,15 @@ import edu.pitt.terminology.util.TerminologyException;
 
 public class AddCuis {
 	
-	HashMap<String, String> meshMap;
-	IndexFinderTerminology term;
+	private HashMap<String, String> meshMap;
+	private IndexFinderTerminology term;
+	private PrintWriter pseudolog;
+	private static final Source[] nobleCoderVocabs = {new Source("MDR"), new Source("SNOMEDCT_US"), new Source("MSH")};
 	
-	public AddCuis() {
+	public AddCuis() throws FileNotFoundException {
 		meshMap = null;
 		term = null;
+		pseudolog = new PrintWriter("../missingCUIs.txt");
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -50,30 +54,33 @@ public class AddCuis {
 		try {
 			
 			BufferedReader in = new BufferedReader(new FileReader(input));
+			PrintWriter out = new PrintWriter(output);
 			List<String> line = new LinkedList<String>(Arrays.asList(in.readLine().split("\t")));
-			PrintWriter out = new PrintWriter(new FileWriter(output));
 			
+			System.out.println("Starting the CUI adder");
 			insert(line, 8, "msh_intervention_CUI");
 			insert(line, 7, "msh_condition_CUI");
 			
-//			insert(line, 2, "text_MedDRA_CUI");
-//			insert(line, 2, "text_SNOMED_CT_CUI");
-//			insert(line, 2, "text_MeSH_CUI");
+			insert(line, 2, "text_MedDRA_CUI");
+			insert(line, 2, "text_SNOMED_CT_CUI");
+			insert(line, 2, "text_MeSH_CUI");
 			
-			System.out.println(line);
+//			System.out.println(line);
 			out.write(outputString(line, "\t") + "\n");			
 			
 			ObjectInputStream meshMapfile = new ObjectInputStream(new FileInputStream("../MeSHHashMap.ser"));
 			meshMap = (HashMap<String, String>)meshMapfile.readObject();
-//			setupNobleCoder();
+			setupNobleCoder();
 			meshMapfile.close();
+			
+			System.out.println("Adding CUIs. This may take take a while.");
 			
 			while(in.ready()) {
 				
 				line = new ArrayList<String>(Arrays.asList(in.readLine().split("\t")));
 				addMesh(line, 8);
 				addMesh(line, 7);
-//				addFromNobleCoder(line);
+				addFromNobleCoder(line);
 				out.write(outputString(line, "\t") + "\n");
 				
 //				insert(line, 2, "MedDRA_CUI");
@@ -81,9 +88,11 @@ public class AddCuis {
 //				insert(line, 2, "RxNorm_CUI");
 				
 			}
-			
+			System.out.println("Finished!");
 			in.close();
 			out.close();
+			pseudolog.close();
+			
 		
 		} catch(IOException e) {
 			System.out.println(e.getMessage());
@@ -100,8 +109,39 @@ public class AddCuis {
 	private void addFromNobleCoder(List<String> line) {
 		String find = line.get(1);
 		try {
+			
 			Concept[] results = term.search(find);
-			System.out.println(results[0].getCodes());
+			if(results.length > 0) {
+				@SuppressWarnings("rawtypes")
+				Map nobleCodes = results[0].getCodes();
+
+				LinkedList<String> missingCUIs = new LinkedList<String>();
+				for(Source vocab : nobleCoderVocabs) {
+					if(nobleCodes.containsKey(vocab))
+						insert(line, 2, (String)nobleCodes.get(vocab));
+					else {
+						insert(line, 2, "");
+						missingCUIs.add(vocab.toString());
+					}
+				}
+				if(missingCUIs.size()>0) {
+					StringBuffer log = new StringBuffer();
+					log.append(find + " missing NobleCoder CUIs: ");
+					int index = 0;
+					for(String voc : missingCUIs) {
+						log.append(voc);
+						if(++index < missingCUIs.size())
+							log.append(", ");
+					}
+					pseudolog.write(log.toString() + "\n");
+				}
+			}
+			else {
+				for(int i=0;i<3;++i) {
+					insert(line, 2, "");
+				}
+				pseudolog.write(find + " not found in NobleCoder." + "\n");
+			}
 		} catch (TerminologyException e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
@@ -110,24 +150,34 @@ public class AddCuis {
 	
 	private void setupNobleCoder() throws IOException {
 
-			term = new IndexFinderTerminology("triads-test-2014");
-			term.setSelectBestCandidate(true);
-			term.setFilterSources(term.getSources("MDR,MSH,SNOMED_CT"));
+			term = new IndexFinderTerminology();
+			term.load("triads-test-Feb2014");
+			term.setIgnoreAcronyms(false);
+			term.setOverlapMode(false);
+			term.setSelectBestCandidate(false);
+			term.setDefaultSearchMethod("best-match");
+			term.setSubsumptionMode(false);
+			term.setFilterSources(term.getSources("MDR,MSH,SNOMEDCT_US"));
 			term.
 				setFilterSemanticType(SemanticType
 								.getSemanticTypes(new String []{
 										"Event",
-										"Pathological Function",
+										"Pathologic Function",
+										"Body Substance",
 										"Functional Concept",
 										"Mental or Behavioral Concept",
+										"Mental or Behavioral Dysfunction",
 										"Finding",
 										"Sign or Symptom",
 										"Individual Behavior",
 										"Disease or Syndrome",
-										"Mental Process"
-																}
+										"Mental Process",
+										"Body Part, Organ, or Organ Component",
+										"Therapeutic or Preventive Procedure",
+															   }
 												)
-										);
+									  );
+			pseudolog.write(term.getSearchProperties().toString() + "\n");
 	}
 	
 	private void addMesh(List<String> line, int index) {
@@ -138,7 +188,7 @@ public class AddCuis {
 				cuis.add(meshMap.get(word));
 			else {
 				cuis.add("");
-				System.out.println(word + " not found in exact match.");
+				pseudolog.write("MESH: No exact match for \"" + word + "\"" + "\n");
 			}
 		}
 		line.add(index, outputString(cuis, "|"));
@@ -171,10 +221,17 @@ public class AddCuis {
 	public static void main(String[] args) {
 		
 		final String inputfile = "../Example-CT.gov-data-v3-v011.csv";
-		final String outputfile = "../Example-CT.gov-data-v3-v011_CUIs_v1.csv";
+		final String outputfile = "../Example-CT.gov-data-v3-v011_CUIs_v2.csv";
 		
-		AddCuis add = new AddCuis();
-		add.addCuis(inputfile, outputfile);
+		AddCuis add;
+		try {
+			add = new AddCuis();
+			add.addCuis(inputfile, outputfile);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 	}
 
