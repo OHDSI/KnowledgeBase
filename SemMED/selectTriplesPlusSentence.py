6e8b27786e119281af731f47853e7049243aaca0
epicstar <myramenisgone@gmail.com>
2014-09-03 09:34:59 -0400

    Took out nobletools from project folder. Now using lib/Textools.jar (https://sourceforge.net/projects/nobletools/) using revision r26.

--- SemMED/selectTriplesPlusSentence.py
+++ SemMED/selectTriplesPlusSentence.py
@@ -14,29 +14,41 @@ TODO: filter this down to triples with:
 
 import mysql.connector as sql
 import connectSEMMED as db
+import pprint
 
 cnx = sql.connect(**db.details())
 cursor = cnx.cursor()
 
 with open('allpredicates.txt', 'w') as fil:
 
+	#query = """
+#SELECT CONCEPT1, PREDICATE, CONCEPT2, SENTENCE_ID, PMID, TYPE, NUMBER, SENTENCE
+#FROM 
+#...TODO: 
+#Q1) all rows from PREDICATION_ARGUMENT with a given predication id
+#Q2) SENTENCE_PREDICATION will give the sentence id that can be used for the table.
+
+#you probably do not want to inner join from predication to PREDICATION_ARGUMENT....
+
+#CONCEPT_SEMTYPE_ID
+
+#"""
 	query = """
-SELECT CONCEPT1, PREDICATE, CONCEPT2, SENTENCE_ID, PMID, TYPE, NUMBER, SENTENCE
-FROM 
-...TODO: 
-Q1) all rows from PREDICATION_ARGUMENT with a given predication id
-Q2) SENTENCE_PREDICATION will give the sentence id that can be used for the table.
-
-you probably do not want to inner join from predication to PREDICATION_ARGUMENT....
-
-CONCEPT_SEMTYPE_ID
-
-"""
+			
+			SELECT * 
+			FROM PREDICATION_AGGREGATE inner join sentence 
+			WHERE
+			()
+			
+			
+			LIMIT 100;
+			
+			"""
 
 	cursor.execute(query)
 
 	for predicate in cursor:
-		fil.write(predicate[0] + '\n')
+		pprint.pprint(predicate, fil)
 
 cursor.close()
 cnx.close()
