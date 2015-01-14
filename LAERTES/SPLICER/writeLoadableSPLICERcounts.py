58ea273caf0b70d28530021a9427873acdd5fcd4
rkboyce <rdb20@pitt.edu>
2015-01-14 07:58:37 -0500

    slight modifications to writeLoadableSPLICERcounts.py to run over the updated SPLICER dataset

--- LAERTES/SPLICER/writeLoadableSPLICERcounts.py
+++ LAERTES/SPLICER/writeLoadableSPLICERcounts.py
@@ -3,7 +3,7 @@
 
 import urllib2, urllib, re, sys
 
-DATAFILE = "test-query-of-counts-12162014.csv" # NOTE: this data comes from CSV export of the following query
+DATAFILE = "test-query-of-counts-01142015.csv" # NOTE: this data comes from CSV export of the following query
 
 ## count data retrieved from the Virtuoso SPARQL endpoint using the
 ## following  query. 
@@ -18,7 +18,8 @@ DATAFILE = "test-query-of-counts-12162014.csv" # NOTE: this data comes from CSV 
 ## $ isql-vt -H localhost -S 1111  -U <user name> -P <password> errors=stdout < /tmp/test.sparql > /tmp/test.out
 ## $ egrep "^[0-9]+ +http.*" /tmp/test.out | tr -s ' ' ',' > /tmp/test-query-of-counts.csv
 ##
-## QUERY (paste into /tmp/test.sparql):
+## QUERY (paste into /tmp/test.sparql -- keep the 'SPARQL' string at the beginning!):
+# SPARQL
 # PREFIX ohdsi:<http://purl.org/net/ohdsi#>
 # PREFIX oa:<http://www.w3.org/ns/oa#>
 # PREFIX meddra:<http://purl.bioontology.org/ontology/MEDDRA/>
@@ -69,7 +70,7 @@ def shortenURL(longurl):
         return urlL[0]
 
 # replace the @IMEDS_DRUG@ and @IMEDS_HOI@ strings with the appropriate values
-TEMPLATE = "http://dbmi-icode-01.dbmi.pitt.edu:8080/sparql?default-graph-uri=&query=PREFIX+ohdsi%3A%3Chttp%3A%2F%2Fpurl.org%2Fnet%2Fohdsi%23%3E%0D%0APREFIX+oa%3A%3Chttp%3A%2F%2Fwww.w3.org%2Fns%2Foa%23%3E%0D%0APREFIX+meddra%3A%3Chttp%3A%2F%2Fpurl.bioontology.org%2Fontology%2FMEDDRA%2F%3E%0D%0APREFIX+ncbit%3A+%3Chttp%3A%2F%2Fncicb.nci.nih.gov%2Fxml%2Fowl%2FEVS%2FThesaurus.owl%23%3E%0D%0APREFIX+foaf%3A+%3Chttp%3A%2F%2Fxmlns.com%2Ffoaf%2F0.1%2F%3E%0D%0APREFIX+dailymed%3A%3Chttp%3A%2F%2Fdbmi-icode-01.dbmi.pitt.edu%2FlinkedSPLs%2Fvocab%2Fresource%2F%3E%0D%0A%0D%0ASELECT+*%0D%0AWHERE+{%0D%0AGRAPH+%3Chttp%3A%2F%2Fpurl.org%2Fnet%2Fnlprepository%2Fohdsi-adr-splicer-poc%3E{%0D%0A+%3Fan+a+ohdsi%3AADRAnnotation%3B%0D%0A+++oa%3AhasBody+%3Fbody%3B%0D%0A+++oa%3AhasTarget+%3Ftarget.%0D%0A%0D%0A+%3Fbody+ohdsi%3AImedsDrug+ohdsi%3A@IMEDS_DRUG@.%0D%0A+%3Fbody+ohdsi%3AMeddrraHoi+meddra%3A@IMEDS_HOI@.%0D%0A%0D%0A+%3Ftarget+oa%3AhasSource+%3FsourceURL.%0D%0A+%3Ftarget+oa%3AhasSelector+%3Fselector.%0D%0A%0D%0A+%3Fselector+dailymed%3AsplSection+%3Fsection.%0D%0A}%0D%0AGRAPH+%3Chttp%3A%2F%2Fdbmi-icode-01.dbmi.pitt.edu%2FlinkedSPLs%2F%3E{%0D%0A+%3Fspl+a+ncbit%3ALabel%3B%0D%0A++foaf%3Ahomepage+%3FsourceURL%3B%0D%0A++%3Fsection+%3Ftext.%0D%0A}%0D%0A}%0D%0ALIMIT+10&format=text%2Fhtml&timeout=0&debug=on"
+TEMPLATE = "http://dbmi-icode-01.dbmi.pitt.edu:8080/sparql?default-graph-uri=&query=%23+drill+down+query+for+a+drug+and+HOI+in+the+SPLICER+ADRAnnotation+graph+joined+with+LinkedSPLs%0D%0APREFIX+ohdsi%3A%3Chttp%3A%2F%2Fpurl.org%2Fnet%2Fohdsi%23%3E%0D%0APREFIX+oa%3A%3Chttp%3A%2F%2Fwww.w3.org%2Fns%2Foa%23%3E%0D%0APREFIX+meddra%3A%3Chttp%3A%2F%2Fpurl.bioontology.org%2Fontology%2FMEDDRA%2F%3E%0D%0APREFIX+ncbit%3A+%3Chttp%3A%2F%2Fncicb.nci.nih.gov%2Fxml%2Fowl%2FEVS%2FThesaurus.owl%23%3E%0D%0APREFIX+foaf%3A+%3Chttp%3A%2F%2Fxmlns.com%2Ffoaf%2F0.1%2F%3E%0D%0APREFIX+linkedspls_vocabulary%3A+%3Chttp%3A%2F%2Fbio2rdf.org%2Flinkedspls_vocabulary%3A%3E%0D%0A%0D%0ASELECT+*%0D%0AWHERE+{%0D%0A+GRAPH+%3Chttp%3A%2F%2Fpurl.org%2Fnet%2Fnlprepository%2Fohdsi-adr-splicer-poc%3E+{%0D%0A++%3Fan+a+ohdsi%3AADRAnnotation%3B%0D%0A+++oa%3AhasBody+%3Fbody%3B%0D%0A+++oa%3AhasTarget+%3Ftarget.%0D%0A%0D%0A+++%3Fbody+ohdsi%3AImedsDrug+ohdsi%3A@IMEDS_DRUG@.%0D%0A+++%3Fbody+ohdsi%3AMeddrraHoi+meddra%3A@IMEDS_HOI@.%0D%0A%0D%0A+++%3Ftarget+oa%3AhasSource+%3FsourceURL.%0D%0A+++%3Ftarget+oa%3AhasSelector+%3Fselector.%0D%0A%0D%0A+++%3Fselector+linkedspls_vocabulary%3AsplSection+%3Fsection.%0D%0A++}%0D%0A+GRAPH+%3Chttp%3A%2F%2Fpurl.org%2Fnet%2Fnlprepository%2Fspl-core-poc-11282014%3E+{%0D%0A++%3Fspl+a+ncbit%3ALabel%3B%0D%0A+++foaf%3Ahomepage+%3FsourceURL%3B%0D%0A+++%3Fsection+%3Ftext.%0D%0A+}%0D%0A}%0D%0A&format=text%2Fhtml&timeout=0&debug=on"
 
 f = open(DATAFILE)
 buf = f.read()
@@ -79,7 +80,7 @@ buf = buf.replace("http://purl.org/net/ohdsi#","").replace("http://purl.bioontol
 l = buf.split("\n") # assumes no header. Format should be count,drug,hoi
 
 i = 0
-for elt in l:
+for elt in l[0:5]:
     i += 1
     (cnt,drug,hoi) = [x.strip() for x in elt.split(",")]
     q = TEMPLATE.replace("@IMEDS_DRUG@",drug).replace("@IMEDS_HOI@",hoi)
