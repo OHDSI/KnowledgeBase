d6d75e490b4cbc5618c69ed107b414ea3a1fcec9
Richard Boyce <ningyifan1212@163.com>
2015-10-10 06:39:52 -0400

    preparing to merge sources using local mapping + calls to the SV if needed

--- LAERTES/Schema/postgresql/mergeCountsFromIntegratedSources.py
+++ LAERTES/Schema/postgresql/mergeCountsFromIntegratedSources.py
@@ -15,6 +15,23 @@ SOURCE_LISTING_FILE="integratedSources.conf"
 DRUG_HOI_DATA_FILE="drug-hoi-evidence-data.tsv"
 CACHED_DRUG_HOI_RELATIONSHIP_FILE="cached_uniq_drug_hoi_relationships.csv"
 DRUG_HOI_RELATIONSHIP_FILE="uniq_drug_hoi_relationships.csv"
+MEDDRA_TO_SNOMED_MAPPING_FILE="../../terminology-mappings/MedDRA-to-SNOMED/LU_MEDDRAPT_TO_SNOMED.txt"
+
+# load the meddra to snomed mapping
+meddra_to_snomed_mapping_d = {}
+inf = open(MEDDRA_TO_SNOMED_MAPPING_FILE,"r")
+l = inf.readline() # skip header
+l = inf.readline()
+while l:
+    s = l.strip()
+    if s == "":
+        break
+
+    (meddra_concept_id,meddra_concept_name,snomed_concept_id,snomed_concept_name) = s.split("\t")
+    meddra_to_snomed_mapping_d[meddra_concept_id] = [meddra_concept_name,snomed_concept_id,snomed_concept_name]
+    
+    l = inf.readline()
+inf.close()
 
 # Enum to help parse the data files
 (KEY,EV_SOURCE_LABEL,MODALITY,EV_SOURCE_ID,STATISTIC,LINKOUT,STATISTIC_TYPE) = range(0,7)
@@ -41,8 +58,10 @@ f.close()
 srcL = [x.strip().split("\t") for x in buf.split("\n")]
 print "INFO: Source data locations:\n\t%s" % srcL
 
-## iterate through each source and write the data to go into drug_hoi_evidence
-## Map the HOI concepts to SNOMED if not already done by the source
+## iterate through each source and write the data to go into
+## drug_hoi_evidence Map the HOI concepts to SNOMED if not already
+## done by the source. Use the local mapping file first, then try to
+## query for the mapping.
 ##
 cntr = 0 # serves as an unique key for the output table ('id')
 drugHoiDataOutF = open(DRUG_HOI_DATA_FILE,'w')
@@ -50,6 +69,8 @@ dhKeyD = {} # used to accumulate all drug-HOI keys so that the drug-hoi relation
 idToSnomedD = {} # cache for mesh or meddra to snomed mapping. The
                  # coding systems use distinct patterns for CUIs so it
                  # should be safe to combine them into one dictionary
+noMappingFoundD = {} # stores concept ids that cannot be mapped by any
+                     # method to prevent repetitive querying
 (B_CONCEPT_ID, B_CONCEPT_NAME, B_CONCEPT_CODE, A_CONCEPT_NAME, A_CONCEPT_CODE) = range(0,5) # enum for vocab query results
 for src in srcL[1:]: # skip header
     print "INFO: loading source: %s" % src[0]
@@ -99,27 +120,42 @@ for src in srcL[1:]: # skip header
                 print "WARNING: could not split out drug and hoi from tpl[KEY]: %s" % tpl[KEY]
                 continue
 
+            if noMappingFoundD.has_key(hoi):
+                print "INFO: HOI concept id has already been found to be un-mappable at the present time %s" % hoi
+                continue
+            
+            mappingFound = False
+            
             rows = []
+            if meddra_to_snomed_mapping_d.has_key(hoi):
+                mappingFound = True
+                rows = [[meddra_to_snomed_mapping_d[hoi][1],meddra_to_snomed_mapping_d[hoi][2],"","",meddra_to_snomed_mapping_d[hoi][0]]]
+                print "INFO: MedDRA concept id %s mapped to SNOMED HOI %s using local mapping" % (hoi,meddra_to_snomed_mapping_d[hoi][1])
+            
             if idToSnomedD.has_key(hoi):
+                mappingFound = True
                 rows = idToSnomedD[hoi]
+                print "INFO: MedDRA concept id %s mapped to SNOMED HOI from cache"
 
-            if src[HOI_VOCAB_ID] == "Mesh" or (src[HOI_VOCAB_ID] == "Mixed" and mixedMapped == False):
+            if (mappingFound == False) and (src[HOI_VOCAB_ID] == "Mesh" or (src[HOI_VOCAB_ID] == "Mixed" and mixedMapped == False)):
                 try:
                     print "INFO: Attempting to map concept id for Mesh coded HOI %s to SNOMED" % hoi
                     cur.execute("""
-SELECT c2.CONCEPT_ID AS SNOMED_CONCEPT_ID, c2.concept_name AS SNOMED_CONCEPT_NAME, c2.concept_code AS SNOMED_CONCEPT_CODE, c1.concept_name AS MESH_CONCEPT_NAME, c1.CONCEPT_CODE AS MESH_CONCEPT_CODE
+SELECT DISTINCT c2.CONCEPT_ID AS SNOMED_CONCEPT_ID, c2.concept_name AS SNOMED_CONCEPT_NAME, c2.concept_code AS SNOMED_CONCEPT_CODE, c1.concept_name AS MESH_CONCEPT_NAME, c1.CONCEPT_CODE AS MESH_CONCEPT_CODE
 FROM concept c1 JOIN concept_relationship cr ON cr.concept_id_1 = c1.CONCEPT_ID
 JOIN CONCEPT c2 ON c2.CONCEPT_ID = cr.CONCEPT_ID_2
   AND c2.invalid_reason IS NULL
   AND c2.vocabulary_id = 'SNOMED'
   AND c2.concept_class_id = 'Clinical Finding'
 WHERE c1.concept_id = '%s'
-AND c1.vocabulary_id = 'Mesh'
-AND c1.CONCEPT_CLASS_ID = 'Condition'
+AND c1.vocabulary_id = 'MeSH'
 AND c1.invalid_reason IS NULL
+AND cr.INVALID_REASON IS NULL
+AND c2.DOMAIN_ID = 'Condition'
 """ % hoi)
                 except Exception as e:
                     print "ERROR: Attempt to map concept id for Mesh HOI to SNOMED failed. Error string: %s" % e
+
                 rows = cur.fetchall()
                 if len(rows) == 0:
                     print "WARNING: Attempt to map concept id for Mesh HOI %s to SNOMED failed because no mapping could be found in the standard vocabulary." % hoi
@@ -127,8 +163,10 @@ AND c1.invalid_reason IS NULL
                         print "WARNING: this case will be tested for other vocabs"
                     else:
                         print "WARNING: this case will NOT be tested for other vocabs"
+                        noMappingFoundD[hoi] = None
                         continue 
                 else:
+                    mappingFound == True
                     if src[HOI_VOCAB_ID] == "Mixed":
                         mixedMapped = True
                         
@@ -136,7 +174,7 @@ AND c1.invalid_reason IS NULL
                     if not idToSnomedD.has_key(hoi):
                         idToSnomedD[hoi] = rows
 
-            if src[HOI_VOCAB_ID] == "MedDRA" or (src[HOI_VOCAB_ID] == "Mixed" and mixedMapped == False):
+            if (mappingFound == False) and (src[HOI_VOCAB_ID] == "MedDRA" or (src[HOI_VOCAB_ID] == "Mixed" and mixedMapped == False)):
                 try:
                     print "INFO: Attempting to map concept id for MedDRA coded HOI '%s' to SNOMED" % hoi
                     # The way this works is it tries to find the
@@ -167,8 +205,11 @@ WHERE z.ROW_NUM = 1
                         print "WARNING: this case will be tested for other vocabs"
                     else:
                         print "WARNING: this case will NOT be tested for other vocabs"
+                        noMappingFoundD[hoi] = None
                         continue 
                 else:
+                    mappingFound == True
+                    
                     if src[HOI_VOCAB_ID] == "Mixed":
                         mixedMapped = True
                         
@@ -176,7 +217,7 @@ WHERE z.ROW_NUM = 1
                     if not idToSnomedD.has_key(hoi):
                         idToSnomedD[hoi] = rows
 
-            if (src[HOI_VOCAB_ID] == "Mixed" and mixedMapped == False):
+            if (mappingFound == False) and ((src[HOI_VOCAB_ID] == "Mixed" and mixedMapped == False)):
                 try:
                     print "INFO: Testing if coded HOI %s is already a SNOMED" % hoi
                     cur.execute("""
@@ -193,6 +234,7 @@ AND c1.concept_id = %s
                 if len(rows) == 0:
                     print "WARNING: Test if concept id HOI %s is SNOMED failed. It's possible that the concept is not in SNOMED or that it's not a Clinical Finding or the concept is invalid according to the standard vocabulary." % hoi
                     print "WARNING: There are no other tests for this concept: %s" % hoi
+                    noMappingFoundD[hoi] = None
                     continue
                 
                 cntr += 1
